d0088b05d2448b5bd3570053ab5ee30071480393
marianovillalonga <78375048+marianovillalonga@users.noreply.github.com>
2023-05-23 23:44:22 -0300

    Formulario para agregar categoria

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -23,6 +23,42 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
             </div>
             <form className={`formulario ${animarModal ? "animar" : 'cerrar'}`}>
                 <legend>Nuevo Gasto</legend>
+                <div className='campo'>
+                    <label htmlFor="nombre">Nombre Gasto</label>
+                    <input 
+                        id='nombre'
+                        type="text" 
+                        placeholder='Añade el nombre del Gasto'
+                    />
+                </div>
+                <div className='campo'>
+                    <label htmlFor="cantidad">Cantidad</label>
+                    <input 
+                        id='cantidad'
+                        type="number" 
+                        placeholder='Añade la cantidad del Gasto ej. 300'
+                    />
+                </div>
+                <div className='campo'>
+                    <label htmlFor="categoria">Categoria</label>
+                    <select 
+                        id="categoria"
+                    >
+                        <option value="">-- Seleccione --</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>
+                        <option value="casa">Casa</option>
+                        <option value="varios">Gastos Varios</option>
+                        <option value="libre">Tiempo libre</option>
+                        <option value="salud">Salud</option>
+                        <option value="suscripciones">Suscripciones</option>
+                    </select>
+                </div>
+
+                <input 
+                    type="submit" 
+                    value="Añadir Gastos"
+                />
             </form>
         </div>
     )
